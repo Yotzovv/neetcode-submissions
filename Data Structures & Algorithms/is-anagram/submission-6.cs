30c38e000c79e98e3d1085648eb7812b5703addb
Yotzovv <ivaylo.yotsov1@gmail.com>
2026-08-04 09:01:58 +0300

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        if (s.Length != t.Length) return false;
+        
+        var sArr = s.ToCharArray();
+        var tArr = t.ToCharArray();
+        Array.Sort(sArr);
+        Array.Sort(tArr);
+        
+        for (var i = 0; i < s.Length; i++) if (sArr[i] != tArr[i]) return false;
+        
+        return true;
+    }
+}
