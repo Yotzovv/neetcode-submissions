fc9caa1472f79a9daa44951effc13cdddc64ef40
Yotzovv <ivaylo.yotsov1@gmail.com>
2026-09-16 13:28:03 +0300

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.cs
@@ -0,0 +1,29 @@
+public class Solution {
+    public bool IsValid(string s) {
+        var stack = new Stack<char>();
+        var closeToOpen = new Dictionary<char, char> {
+            { ')', '(' },
+            { ']', '[' },
+            { '}', '{' }
+        };
+        
+        foreach (char c in s) {
+            if (!closeToOpen.ContainsKey(c))
+            {
+                stack.Push(c);
+                continue;
+            }
+
+            if (stack.Count > 0 && stack.Peek() == closeToOpen[c])
+            {
+                stack.Pop();
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return stack.Count == 0;
+    }
+}
